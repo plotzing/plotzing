7e08dcc97cda17a58cd6fb306b9f4e749d350f09
plotzing <156881251+plotzing@users.noreply.github.com>
2024-01-31 21:08:52 -0800

    Updated Save Function
    Changed the default dpi to be 500 when using the save_graph() function.

--- R/Saving_plots_function.R
+++ R/Saving_plots_function.R
@@ -1,6 +1,6 @@
 #### SAVING PLOTS ####
 #Save graphs#
-save_graph<-function(nameoffile,setwidth=NULL,setheight=NULL,setunits=NULL,width=NA,height=NA,units="in"){
+save_graph<-function(nameoffile,setwidth=NULL,setheight=NULL,setunits=NULL,width=NA,height=NA,units="in",dpi=500){
   require(ggplot2)
   if(!is.null(setwidth)){
     width<-setwidth
@@ -11,7 +11,7 @@ save_graph<-function(nameoffile,setwidth=NULL,setheight=NULL,setunits=NULL,width
   if(!is.null(setunits)){
     units<-setunits
   }
-  ggsave(filename = nameoffile,plot=last_plot(),units=units,height=height,width=width)
+  ggsave(filename = nameoffile,plot=last_plot(),units=units,height=height,width=width,dpi=dpi)
   message("Your graph has been saved in the following location:")
   message(getwd())
 }
